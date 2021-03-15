39c25eadd2b5eace5c118c16e1952b772ecd9690
Goran <goranfrance@hotmail.com>
2021-03-15 16:41:41 +0000

    afd

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from "prop-types"
 
 export class Navbar extends Component {
+    static defaultProps = {
+        title: "Github Finder",
+        icon: "fab fa-github"
+    }
     render() {
         return (
             <div>
-                <h1>Navbar</h1>
+                <h1>
+  <i className={this.props.icon}></i> 
+  {this.props.title}
+                </h1>
             </div>
         )
     }
