0e3f359f64b68afb5bd311c1a4e262987d308b99
Goran <goranfrance@hotmail.com>
2021-03-15 00:31:23 +0000

    fd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello From React</h1>
-    </div>
-  );
+class App extends Component {
+    render() {
+    return (
+      <div className="App">
+       <h1>Hello From React</h1>
+      </div>
+    );
+  } 
 }
 
 export default App;
