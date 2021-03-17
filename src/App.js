907d6fd259dba89217f448c1516dc17e82dbb4ca
Goran <goranfrance@hotmail.com>
2021-03-17 23:00:06 +0000

    fa

--- src/App.js
+++ src/App.js
@@ -19,12 +19,16 @@ class App extends Component {
 
       this.setState({ users: res.data.items, loading: false})
     }
+    //CLEAR USERS//
+    clearUsers = () => {
+      this.setState({ users: [], loading: false })
+    }
     render() {    
     return (
       <div className="App">
       <Navbar />
       <div className="container">
-      <Search searchUsers={this.searchUsers} />
+      <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false} />
       <Users loading={this.state.loading} users={this.state.users}/>
       </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ export class Search extends Component {
         text: ""
     }
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
     onSubmit = (e) => {
         e.preventDefault()
@@ -24,6 +26,8 @@ export class Search extends Component {
                     <input type="text" name="text" placeholder="Search Users.." value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
+                
             </div>
         )
     }
