1cb0e01428244cddfd0ca2486ad5f90fd17c15ce
Goran <goranfrance@hotmail.com>
2021-03-20 00:53:30 +0000

    afds

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     /// Get a single github user
     getUser = async (username) => {
        this.setState({ loading: true })
-      const res = await axios.get(`https://api.github.com/search/${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+      const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
       this.setState({ user: res.data, loading: false})
     }
